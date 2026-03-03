2fe44993b4a77a2f7ad7d032f8be5d731d33444d
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-03 23:31:13 +0530

    Checking Even and Odd in a Division Problem using if...else Statement

--- If_Else2.java
+++ If_Else2.java
@@ -7,19 +7,18 @@ class Numbers
 {
     double num1, num2, res;
 
-    public void input(Scanner scn)
+    public void input(Scanner scn)    //function to input the nos. for Division
     {
         System.out.print("Enter the 1st no.: ");
-        num1 = scn.nextInt();
-
+        num1 = scn.nextDouble();
         System.out.print("Enter the 2nd no.: ");
-        num2 = scn.nextInt();
+        num2 = scn.nextDouble();
 
-        res = num1/num2;
+        res = num1/num2;    //Formula of Division
     }
-    public void check()
+    public void check()     //Function to check whether the output is Even or Odd
     {
-        if(res%2==0)
+        if(res%2==0)    //condition
         {
             System.out.println("Output is an Even no.");
         }
@@ -34,9 +33,10 @@ public class If_Else2
     public static void main(String args[])
     {
         Scanner scn = new Scanner(System.in);
-        Numbers obj = new Numbers();
-        obj.input(scn);
-        obj.check();
+        Numbers obj = new Numbers();    //Object creation for "Numbers" class 
+        obj.input(scn);    //accessing the input function 
+        obj.check();       //accessing the output function
+
+        scn.close();
     }
-    
 }
---
+++ If_Else3.java
@@ -0,0 +1,47 @@
+/*Comparing the equality of 2 nos. using if...else Statements */
+
+import java.util.Scanner;    //Importing the Scanner class
+
+class Values
+{
+    int a, b;
+
+    public void input(Scanner scn)    //function to input the nos.
+    {
+        System.out.print("Enter the 1st no.: ");
+        a = scn.nextInt();
+
+        System.out.print("Enter the 2nd no.: ");
+        b = scn.nextInt();
+    }
+    public void checks()    //function to check the Equality of both the nos.
+    {
+        if(a==b)    //condition
+        {
+            System.out.println("a and b are Equal");
+        }
+        else
+        {
+            if(a>b)    //condition
+            {
+                System.out.println("a is greater than b");
+            }
+            else
+            {
+                System.out.println("a is lesser than b");
+            }
+        }
+    }
+}
+public class If_Else3
+{
+    public static void main(String args[])
+    {
+        Scanner scn = new Scanner(System.in);
+        Values obj = new Values();    //object creation for "Values" class
+        obj.input(scn);    //accessing the input function
+        obj.checks();      //accessing the output function
+
+        scn.close();
+    }
+}
