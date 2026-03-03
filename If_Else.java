b421d3841bbb923cb9bc34d38aa0d4cc5069de0a
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-03 22:56:23 +0530

    Checking Eligibility of a Voter using if...else Statements

---
+++ If_Else.java
@@ -0,0 +1,35 @@
+/*Checking whether the user is eligible to vote or not, using if...else statement */
+import java.util.Scanner;
+class Voter 
+{
+    int age;
+
+    public void input(Scanner scn)    //function to input the age
+    {
+        System.out.print("Enter your age: ");
+        age = scn.nextInt();
+    }
+    public void ageCheck()    //function to check the age
+    {
+        if (age>=18)    //condition
+        {
+            System.out.println("You are Eligible to Vote!!");    //for true statement
+        }
+        else    //for false statement
+        {
+            System.out.println("Sorry, you're not eligible to vote!!");
+        }
+    }
+}
+public class If_Else    //class to access all the functions
+{
+    public static void main(String args[])
+    {
+        Scanner scn = new Scanner(System.in);
+        Voter obj = new Voter();    //Creating Class Voter's objects
+        obj.input(scn);    //object to access the input function
+        obj.ageCheck();    //object to access the output function
+
+        scn.close();
+    }    
+}
