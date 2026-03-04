7a072d0ccf99b22a6891db3512bdbc87a7512191
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-04 22:22:43 +0530

    Printing numbers from 0-10 using for loop

---
+++ Number.java
@@ -0,0 +1,11 @@
+/*printing numbers from 0 to 10 using for loop*/
+
+public class Number {
+    public static void main(String args[])
+    {
+        for(int i=0; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
