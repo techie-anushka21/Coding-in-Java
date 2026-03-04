1f844a3eedda234434d5731f38d15ead7570746c
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-04 23:41:42 +0530

    Printing nos. from 1-10 using do while loop

---
+++ NumberDW.java
@@ -0,0 +1,13 @@
+public class NumberDW
+{
+    public static void main(String args[])
+    {
+        int i=0;    //initialization
+        do
+        {
+            i++;    //updation
+            System.out.println(i);
+        }
+        while(i<11);    //condition
+    }    
+}
