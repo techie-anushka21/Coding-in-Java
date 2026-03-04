d99b8d62cae307420e03c19f5045e33e962ec6df
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-04 22:21:07 +0530

    Creating Inverted Right-angled Triangle using for loop

---
+++ ForStars3.java
@@ -0,0 +1,16 @@
+/*printing a Inverted Right-angled Triangle in Java using for loop*/
+public class ForStars3
+{
+    public static void main(String args[])
+    {
+        int n = 5;    //no. of rows
+        for(int i=n; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("*");    //print i stars in each row
+            }
+            System.out.println();
+        }
+    }
+}
