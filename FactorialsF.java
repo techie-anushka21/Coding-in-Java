008f240f8fe1936b01d36fe38c79fc3be5c1b6ef
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-05 02:27:42 +0530

    Printing 4's Factorial using for loop

---
+++ FactorialsF.java
@@ -0,0 +1,23 @@
+/*Factorial of a no., using for loop */
+class Facts
+{
+    int n=4;
+
+    public void fact()    //function for operation
+    {
+        int res=1;    //a temporary variable
+        for(int i=1; i<=n; i++)
+        {
+            res = res*i;    //method for getting the solution
+        }
+        System.out.println(res);
+    }
+}
+public class FactorialsF
+{
+    public static void main(String args[])
+    {
+        Facts obj = new Facts();    //object creation
+        obj.fact();                 //accessng the object
+    }
+}
