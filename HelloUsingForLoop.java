424fbb8a9b4f352f937b8fbbacfda484894a859c
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-04 21:06:29 +0530

    Printing Hello World 5 times using for loop

---
+++ HelloUsingForLoop.java
@@ -0,0 +1,24 @@
+/*for loop in Java */
+/*for(initialization; condition; updation)
+{
+    //statement(s);
+} */
+/*print "Hello World!" in Java 5 times */
+class Real
+{
+    public void loops()    //function for storing the for loop 
+    {
+        for(int i=1; i<=5; i++)    //for loop 
+        {
+            System.out.println("Hello World!");  //message to implement for loop 
+        }
+    }
+}
+public class HelloUsingForLoop
+{
+    public static void main(String args[])
+    {
+        Real obj = new Real();    //object creation for "Real" class
+        obj.loops();    //accessing the object
+    }
+}
